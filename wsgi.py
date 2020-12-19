7c80943b62ca03b9e492e6dae36f493ab7d5558d
Erik Carrillo <hell-dxd@hotmail.com>
2020-12-18 19:41:02 -0600

    comando wsgi

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,5 @@
 from portal import app
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', 8080, debug=True)
+    #app.run() #descomentar cuando el comando : uwsgi --socket 0.0.0.0:8080 --protocol=http -w wsgi:app
+    app.run('0.0.0.0', 8080, debug=True) #comentar cuando el comando : uwsgi --socket 0.0.0.0:8080 --protocol=http -w wsgi:app
